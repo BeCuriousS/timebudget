d02f2f2172c10d609dc7ed54ba2ffaf033ddf313
Matthieu Scherpf <matthieu.scherpf@tu-dresden.de>
2022-07-22 10:15:20 +0200

    added set_active; added distinct naming

--- timebudget/timebudget.py
+++ timebudget/timebudget.py
@@ -27,13 +27,15 @@ from typing import Callable, Optional, Union
 import warnings
 
 __all__ = [
-    'timebudget', 
-    'annotate', 
-    'report', 
+    'timebudget',
+    'annotate',
+    'report',
     'set_quiet',
+    'set_active',
 ]
 
-def ms_format(milliseconds:float) -> str:
+
+def ms_format(milliseconds: float) -> str:
     """Slightly smart formating of an elapsed time
     """
     assert milliseconds >= 0
@@ -49,8 +51,9 @@ class TimeBudgetRecorder():
     This is mostly used through annotation and with-block helpers.
     """
 
-    def __init__(self, quiet_mode:bool=False):
+    def __init__(self, quiet_mode: bool = False, active: bool = False):
         self.quiet_mode = quiet_mode
+        self.is_active = active
         self.reset()
         self.out_stream = sys.stdout
 
@@ -61,25 +64,27 @@ class TimeBudgetRecorder():
         self.elapsed_total = defaultdict(float)  # float defaults to 0
         self.elapsed_cnt = defaultdict(int)  # int defaults to 0
 
-    def _print(self, msg:str):
+    def _print(self, msg: str):
         self.out_stream.write(msg)
         self.out_stream.write("\n")
         self.out_stream.flush()
 
-    def start(self, block_name:str):
+    def start(self, block_name: str):
         if block_name in self.start_times:
             # End should clear out the record, so something odd has happened here.
             # try/finally should prevent this, but sometimes it doesn't.
-            warnings.warn(f"timebudget is confused: timebudget.start({block_name}) without end")
+            warnings.warn(
+                f"timebudget is confused: timebudget.start({block_name}) without end")
         self.start_times[block_name] = time.time()
 
-    def end(self, block_name:str, quiet:Optional[bool]=None) -> float:
+    def end(self, block_name: str, quiet: Optional[bool] = None) -> float:
         """Returns number of ms spent in this block this time.
         """
         if quiet is None:
             quiet = self.quiet_mode
         if block_name not in self.start_times:
-            warnings.warn(f"timebudget is confused: timebudget.end({block_name}) without start")
+            warnings.warn(
+                f"timebudget is confused: timebudget.end({block_name}) without start")
             return float('NaN')
         elapsed = 1000*(time.time() - self.start_times[block_name])
         self.elapsed_total[block_name] += elapsed
@@ -89,7 +94,7 @@ class TimeBudgetRecorder():
             self._print(f"{block_name} took {ms_format(elapsed)}")
         return elapsed
 
-    def report(self, percent_of:str=None, reset:bool=False):
+    def report(self, percent_of: str = None, reset: bool = False):
         """Prints a report summarizing all the times recorded by timebudget.
         If percent_of is specified, then times are shown as a percent of that function.
         If `reset` is set, then all stats will be cleared after this report.
@@ -113,38 +118,50 @@ class TimeBudgetRecorder():
                 avg = res['total'] / total_cnt
                 pct = 100.0 * res['total'] / total_elapsed
                 avg_cnt = res['cnt'] / total_cnt
-                self._print(f"{res['name']:>25s}:{pct: 6.1f}% {avg: 8.2f}ms/cyc @{avg_cnt: 8.1f} calls/cyc")
+                self._print(
+                    f"{res['name']:>60s}:{pct: 6.1f}% {avg: 8.2f}ms/cyc @{avg_cnt: 8.1f} calls/cyc")
         else:
             self._print("timebudget report...")
             for res in results:
-                self._print(f"{res['name']:>25}:{res['avg']: 8.2f}ms for {res['cnt']: 6d} calls")
+                self._print(
+                    f"{res['name']:>60s}:{res['avg']: 10.2f}ms for {res['cnt']: 6d} calls with overall {res['cnt']*res['avg']: 10.2f}")
         if reset:
             self.reset()
 
 
-_default_recorder = TimeBudgetRecorder()  
+_default_recorder = TimeBudgetRecorder()
 
 
-def annotate(func:Callable, quiet:Optional[bool]):
+def annotate(func: Callable, quiet: Optional[bool]):
     """Annotates a function or code-block to record how long the execution takes.
     Print summary with timebudget.report
     """
-    name = func.__name__
+    func_name = func.__name__
+
     @wraps(func)
     def inner(*args, **kwargs):
-        _default_recorder.start(name)
+        cl_name = ''
+        # check if first function argument is 'self', i.e. a class instance
+        if len(args):
+            if hasattr(args[0], '__class__'):
+                cl_name = args[0].__class__.__name__ + '.'
+        name = cl_name + func_name
+        if _default_recorder.is_active:
+            _default_recorder.start(name)
         try:
             return func(*args, **kwargs)
         finally:
-            _default_recorder.end(name, quiet)
+            if _default_recorder.is_active:
+                _default_recorder.end(name, quiet)
     return inner
+
 
 class _timeblock():
     """Surround a code-block with a timer as in
         with timebudget('loadfile'):
     """
 
-    def __init__(self, name:str, quiet:Optional[bool]):
+    def __init__(self, name: str, quiet: Optional[bool]):
         self.name = name
         self.quiet = quiet
 
@@ -155,20 +172,27 @@ class _timeblock():
         _default_recorder.end(self.name, self.quiet)
 
 
-def annotate_or_with_block(func_or_name:Union[Callable, str], quiet:Optional[bool]=None):
+def annotate_or_with_block(func_or_name: Union[Callable, str], quiet: Optional[bool] = None):
     if callable(func_or_name):
         return annotate(func_or_name, quiet)
     if isinstance(func_or_name, str):
         return _timeblock(func_or_name, quiet)
-    raise RuntimeError("timebudget: Don't know what to do. Either @annotate or with:block")
+    raise RuntimeError(
+        "timebudget: Don't know what to do. Either @annotate or with:block")
 
 
-def set_quiet(quiet:bool=True):
+def set_quiet(quiet: bool = True):
     """Tell timebudget not to print time measurements on every call, but instead
     to save them only for a report.  
     Alternately, you can reverse this by calling set_quiet(False).
     """
     _default_recorder.quiet_mode = quiet
+
+
+def set_active(active: bool = True):
+    """Activate or deactivate the tracking with timebudget.
+    """
+    _default_recorder.is_active = active
 
 
 # Create shortcuts for export
@@ -177,14 +201,16 @@ report = _default_recorder.report
 timebudget.report = report
 timebudget.__doc__ = __doc__
 timebudget.set_quiet = set_quiet
+timebudget.set_active = set_active
 timebudget._default_recorder = _default_recorder
 
-def report_at_exit(block_name:str=None):
+
+def report_at_exit(block_name: str = None):
     if block_name:
         atexit.register(lambda: report(block_name))
     else:
         atexit.register(report)
 
+
 timebudget.report_at_exit = report_at_exit
 timebudget.report_atexit = report_at_exit  # for backwards compat with v0.6
-
